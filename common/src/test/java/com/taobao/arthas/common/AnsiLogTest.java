87d64f2134d8d3e79d4e1f49eea00471ffe4f4d1
Diffblue Assistant <42613982+diffblue-assistant@users.noreply.github.com>
2019-08-07 09:51:13 +0100

    Add Unit Tests for the following classes:
    com.taobao.arthas.common.AnsiLogTest
    com.taobao.arthas.common.UsageRenderTest
    com.taobao.arthas.core.util.ObjectUtilsTest
    
    These tests were written using Diffblue Cover

---
+++ common/src/test/java/com/taobao/arthas/common/AnsiLogTest.java
@@ -0,0 +1,244 @@
+package com.taobao.arthas.common;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import com.taobao.arthas.common.AnsiLog;
+import com.taobao.arthas.common.OSUtils;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.logging.Level;
+
+@RunWith(PowerMockRunner.class)
+public class AnsiLogTest {
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Rule public final Timeout globalTimeout = new Timeout(10000);
+
+  /* testedClasses: AnsiLog */
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void blackInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.black(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void blueInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.blue(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void cyanInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.cyan(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void enableColorOutputFalse() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final boolean actual = AnsiLog.enableColor();
+
+    // Assert result
+    Assert.assertFalse(actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void greenInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.green(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void levelOutputNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final Level actual = AnsiLog.level();
+
+    // Assert result
+    Assert.assertNull(actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void magentaInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.magenta(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void redInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.red(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void whiteInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.white(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @PrepareForTest({AnsiLog.class, OSUtils.class, System.class})
+  @Test
+  public void yellowInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String msg = "3";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = AnsiLog.yellow(msg);
+
+    // Assert result
+    Assert.assertEquals("3", actual);
+  }
+}
---
+++ common/src/test/java/com/taobao/arthas/common/UsageRenderTest.java
@@ -0,0 +1,46 @@
+package com.taobao.arthas.common;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import com.taobao.arthas.common.OSUtils;
+import com.taobao.arthas.common.UsageRender;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class UsageRenderTest {
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Rule public final Timeout globalTimeout = new Timeout(10000);
+
+  /* testedClasses: UsageRender */
+  // Test written by Diffblue Cover.
+  @PrepareForTest({UsageRender.class, OSUtils.class, System.class})
+  @Test
+  public void renderInputNotNullOutputNotNull() throws Exception {
+
+    // Setup mocks
+    PowerMockito.mockStatic(System.class);
+    PowerMockito.mockStatic(OSUtils.class);
+
+    // Arrange
+    final String usage = "BAZ";
+    PowerMockito.doReturn(false).when(OSUtils.class);
+    OSUtils.isCygwinOrMinGW();
+    PowerMockito.when(System.console()).thenReturn(null);
+
+    // Act
+    final String actual = UsageRender.render(usage);
+
+    // Assert result
+    Assert.assertEquals("BAZ", actual);
+  }
+}
